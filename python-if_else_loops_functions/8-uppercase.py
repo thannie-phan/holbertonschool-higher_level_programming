f7a76eef2e1a726f7192811a60d913e909202827
thannie-phan <thannie-phan@gmail.com>
2025-09-01 11:37:48 +0000

    task8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            char = chr(ord(char) - 32)
+        print(char, end='')
+    print('')
