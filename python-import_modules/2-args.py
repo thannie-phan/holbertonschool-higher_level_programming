20d9e8fd4f73acc91ac96f64d0f8a80a40b0b5a9
thannie-phan <thannie-phan@gmail.com>
2025-09-02 00:17:22 +0000

    task2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    argc = len(argv)
+    
+    if argc == 0:
+        print('0 arguments.')
+    else:
+        print(f'{argc} argument:')
+        
+    for num_args, arg in enumerate(argv, start=1):
+        print(f'{num_args}: {arg}')
