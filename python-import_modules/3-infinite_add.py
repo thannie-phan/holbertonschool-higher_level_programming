305d8a92b78e2ef3999418c01e65829dbfba4890
thannie-phan <thannie-phan@gmail.com>
2025-09-02 01:59:55 +0000

    task5

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,10 +2,5 @@
 import sys
 if __name__ == '__main__':
     argv = sys.argv[1:]
-    argc = len(argv)
-
-    if argc == 0:
-        print('0')
-    else:
-        total = sum(int(arg) for arg in argv)
-        print(total)
+    total = sum(int(arg) for arg in argv)
+    print(total)
