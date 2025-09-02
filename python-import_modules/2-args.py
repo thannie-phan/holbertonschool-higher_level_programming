898fa65b86571750dec984178710f9dc23740c28
thannie-phan <thannie-phan@gmail.com>
2025-09-02 00:20:52 +0000

    task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,13 +3,13 @@ import sys
 if __name__ == '__main__':
     argv = sys.argv[1:]
     argc = len(argv)
-    
+
     if argc == 0:
         print('0 arguments.')
     elif argc == 1:
         print(f'{argc} argument:')
     else:
-        print(f'{argc} argument:')
-        
+        print(f'{argc} arguments:')
+
     for num_args, arg in enumerate(argv, start=1):
         print(f'{num_args}: {arg}')
