d563b654cc6736843b83286fba09a780dddb5d88
thannie-phan <thannie-phan@gmail.com>
2025-09-08 21:54:34 +0000

    add isinstance not string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or isinstance(roman_string, str) is False:
         return 0
     roman_numerals = {
         'I': 1,
