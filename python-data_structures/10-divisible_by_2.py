053ebc86702cabd9c566e470292595601cd5c20c
thannie-phan <thannie-phan@gmail.com>
2025-09-08 05:32:01 +0000

    assign to new_list

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new_list = my_list.copy()
     for index, num in enumerate(my_list):
         if num % 2 == 0:
-            my_list[index] = True
-        else:
-            my_list[index] = False
-    return my_list
+            new_list[index] = True
+        elif num % 2 != 0:
+            new_list[index] = False
+    return new_list
 # End of function
