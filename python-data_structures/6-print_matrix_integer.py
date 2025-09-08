58f6eb54a44e7cd4cafe3e31900febedc9fd007e
thannie-phan <thannie-phan@gmail.com>
2025-09-08 04:08:53 +0000

    add if conditioning to print if at last column of row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(*row)
+        for element in row:
+            if element != row[-1]:
+                print('{:d}'.format(element), end=' ')
+            else:
+                print('{:d}'.format(element), end='')
+        print()
             # End of function
