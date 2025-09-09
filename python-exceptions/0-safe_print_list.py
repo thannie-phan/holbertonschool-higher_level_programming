14e43b5866758a4d7cb1c957474143316f5eeba9
thannie-phan <thannie-phan@gmail.com>
2025-09-09 00:55:28 +0000

    task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for element in range(x):
+            print(my_list[element], end='')
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
 
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
-
+        return count
