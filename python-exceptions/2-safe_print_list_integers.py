cd48c6dd066593878fd8e51fbdfdd0174e99f94f
thannie-phan <thannie-phan@gmail.com>
2025-09-09 04:48:18 +0000

    task2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for element in range(x):
+        try:
+            print(my_list[element], end='')
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        except (IndexError):
+            raise
+    print()
+    return count
