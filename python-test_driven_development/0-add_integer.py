6bcde1bea0dedac793dac497379efa806638622a
thannie-phan <thannie-phan@gmail.com>
2025-09-11 00:16:03 +0000

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-add_integer = __import__('0-add_integer').add_integer
+"""The function adds 2 integers."""
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+def add_integer(a, b=98):
+    """Adds two integer"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
