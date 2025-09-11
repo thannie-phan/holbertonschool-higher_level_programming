5aad0bbecfafff20b56b18ab7e1e8e4f3d94e7dd
thannie-phan <thannie-phan@gmail.com>
2025-09-11 02:15:07 +0000

    task2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
+"""The function div all elements in matrix."""
+
 def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix
+
+    Args:
+        matrix (int or float): the matrix
+        div (int or float): divisor
+
+    Returns:
+        list: new matrix as result of the division
+    """
     result = []
-    try:
-        for row in range (len(matrix)):
-    except:
-        
-    for element in range(len(matrix)):   
-        try:
-            value = matrix[element] / div
-        except (ZeroDivisionError):
-            print("division by 0")
-            value = 0
-        except (TypeError, ValueError):
-            print("wrong type")
-            value = 0
-        except (IndexError):
-            print("out of range")
-            value = 0
-        finally:
-            result.append(value)
+    for row in range (len(matrix)):
+        if not isinstance(matrix[row], list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(matrix[row]) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for col in range(len(matrix[row])):
+            if not isinstance(matrix[row][col], (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            try:
+                value = matrix[row][col] / div
+                format_value = float('{:.2f}'.format(value))
+            except div == 0:
+                ZeroDivisionError('division by zero')
+            except not isinstance(div, (int, float)):
+                TypeError("div must be a number")
+            finally:
+                result.append(format_value)
     return result
