edc5de1f4a41e85d91976e955afb6aebc0e5eeeb
thannie-phan <thannie-phan@gmail.com>
2025-09-11 01:22:23 +0000

    pycode

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """The function adds 2 integers."""
 
+
 def add_integer(a, b=98):
     """
     Adds two integer
-    
-    Args: 
+
+    Args:
         a (int or float): first int
         b (int or float): second int, default is 98
-    
+
     Returns:
         int: sum of a and b as int
     """
