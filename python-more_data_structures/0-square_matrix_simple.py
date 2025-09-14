5ece02d6feaf7cbc2a217dcaa21a11dd01ed460f
thannie-phan <thannie-phan@gmail.com>
2025-09-14 11:50:38 +0000

    task0 pycode

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [row.copy() for row in matrix]
     for row in range(len(new_matrix)):
         for col in range(len(new_matrix[row])):
-            new_matrix[row][col] **= 2       
+            new_matrix[row][col] **= 2 
     return new_matrix
 # End of function
