a0093d133ca966fc09b7ecb421e8d76168124c9e
thannie-phan <thannie-phan@gmail.com>
2025-09-15 00:38:36 +0000

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a class Square."""
+
+
+class Square:
+    """This class defines a square by its size."""
+    def __init__(self, size):
+        self.__size = size
