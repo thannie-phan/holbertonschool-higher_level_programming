2cb733b13b3c757183786a3ba21fad28345eb763
thannie-phan <thannie-phan@gmail.com>
2025-09-15 01:59:01 +0000

    task5

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a class Square."""
+
+
+class Square:
+    """This class defines a square by its size."""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter for the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates and returns the area of the square."""
+        return self.__size ** 2
+    
+    def my_print(self):
+        """Prints the square using the '#' character."""
+        if self.__size == 0:
+            print("")
+
+        for column in range(self.__size):
+            for row in range(self.__size):
+                print('#', end='')
+            print()
