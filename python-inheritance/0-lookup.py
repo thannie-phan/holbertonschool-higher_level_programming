ded23a75897fa36e079269b3166ff0b729c39c7a
thannie-phan <thannie-phan@gmail.com>
2025-09-22 05:04:48 +0000

    task0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
+"""This module defines a function to lookup attributes and methods of an object."""
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object.
 
-okup(objobobobj
-        bj:wq
-    qqqqqqq
+    Args:
+        obj (any): The object to inspect.
+    Returns:
+        list: A list of strings representing the attributes and methods of the object.
+    """
+    return dir(obj)
