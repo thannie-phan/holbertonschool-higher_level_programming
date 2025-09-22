86714e58751867a4bfc46621845258d1ac0031d3
thannie-phan <thannie-phan@gmail.com>
2025-09-22 11:56:28 +0000

    task9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,3 +16,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
+    def area(self):
+        """This returns the area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """This returns the print() and str()
+        representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
