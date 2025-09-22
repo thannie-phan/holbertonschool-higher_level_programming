5991e0ec8d7658c15dcd02a9298d9c618b6a73bc
thannie-phan <thannie-phan@gmail.com>
2025-09-22 05:27:36 +0000

    task0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a function to lookup attributes and methods of an object."""
+"""This module defines a func to lookup attributes, methods of obj."""
+
+
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object.
+    """Returns list of available attributes and methods of an obj.
 
     Args:
         obj (any): The object to inspect.
     Returns:
-        list: A list of strings representing the attributes and methods of the object.
+        list: list of str rep attributes and methods of the obj
     """
     return dir(obj)
