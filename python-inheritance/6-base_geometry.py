b1872bf604f847b8b3d8af277546833b4c9ccb99
thannie-phan <thannie-phan@gmail.com>
2025-09-22 08:46:19 +0000

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines class base geometry."""
+
+
+class BaseGeometry:
+    """This defines empty class base geometry. """
+    pass
+
+    def area(self):
+        """Raises an Exception with the message area() is
+        not implemented"""
+        raise Exception("area() is not implemented")
