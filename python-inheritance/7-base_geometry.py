ece145b0ee28cf8bdd420e99eeb6d9a293714587
thannie-phan <thannie-phan@gmail.com>
2025-09-22 08:54:48 +0000

    task7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines class base geometry."""
+
+
+class BaseGeometry:
+    """This defines empty class base geometry. """
+    pass
+
+    def area(self):
+        """Raises an Exception with the message area() is
+        not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This validates value"""
+        if isinstance(value, int) is False:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
