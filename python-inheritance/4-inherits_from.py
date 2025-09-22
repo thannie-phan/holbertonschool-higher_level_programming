c86d599fb63c6558c7166fbfff6613d6a11c7246
thannie-phan <thannie-phan@gmail.com>
2025-09-22 08:36:26 +0000

    task4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""This module returns T if obj is instance of class that inherited
-from the specified class. F otherwise"""
+"""This module returns True if obj is instance of class that
+inherited the specified class. False otherwise"""
 
 
 def inherits_from(obj, a_class):
-    """This module returns T if obj is of class or inherited from
-specified class. F otherwise """
+    """This module returns T if obj is instance of class that
+    inherited specified class. F otherwise """
     if isinstance(obj, a_class) and not type(obj) is a_class:
         return True
     return False
