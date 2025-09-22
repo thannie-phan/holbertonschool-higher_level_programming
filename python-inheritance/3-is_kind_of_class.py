050218766b6bef95b5c19f7b3c9ba233c7cb9f91
thannie-phan <thannie-phan@gmail.com>
2025-09-22 08:26:19 +0000

    task3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""This module returns True if obj is instance of class or inherited class. F otherwise"""
+"""This module returns T if obj is instance of class/inherited class.
+F otherwise"""
 
 
 def is_kind_of_class(obj, a_class):
-    """This module returns True if obj is of class or inherited class. F otherwise """
+    """This module returns T if obj is of class or inherited class.
+F otherwise """
     if type(obj) is a_class or isinstance(obj, a_class):
         return True
     return False
