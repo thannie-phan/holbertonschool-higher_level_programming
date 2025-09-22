20eec9da41ff7a4a5551cc8de19832fc4daa5a6b
thannie-phan <thannie-phan@gmail.com>
2025-09-22 12:14:45 +0000

    task10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines class square."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This defines class Square. """
+
+    def __init__(self, size):
+        """This defines the size of square."""
+
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """This returns the area of square"""
+        return super().area()
+ 
