a29f0840dd4d6c36a9e173a265b5879842e2fab6
thannie-phan <thannie-phan@gmail.com>
2025-09-24 23:31:19 +0000

    task4

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module defines class FlyingFish."""
+
+class Fish:
+    """This defines class Fish. """
+
+    def swim(self):
+        """ abstract method swim."""
+        print('The fish is swimming')
+    
+    def habitat(self):
+        """ abstract method habitat."""
+        print('The fish lives in water')
+
+class Bird:
+    """This defines class Bird. """
+
+    def fly(self):
+        """ abstract method swim."""
+        print('The bird is flying')
+    
+    def habitat(self):
+        """ abstract method habitat."""
+        print('The bird lives in the sky')
+
+class FlyingFish(Bird, Fish):
+    """This defines class FlyingFish. """
+
+    def fly(self):
+        """ abstract method fly."""
+        print('The flyingfish is soaring')
+    
+    def habitat(self):
+        """ abstract method habitat."""
+        print('The flying fish lives both in water and the sky!')
+    
+    def swim(self):
+        """ abstract method swim."""
+        print('The flyingfish is swimming')
