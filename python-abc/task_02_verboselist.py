c276f0b1984c496bf362e1b91167d1a9cc6a2a02
thannie-phan <thannie-phan@gmail.com>
2025-09-24 03:17:58 +0000

    task2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines class VerboseList."""
+
+
+class VerboseList(list):
+    """This defines class VerboseList from list. """
+    def append(self, item):
+        """ override append."""
+        super().append(item)
+        print(f'Added [{item}] to the list.')
+
+    def extend(self, item):
+        """ override extend."""
+        super().extend(item)
+        print(f'Extended the list with [{len(item)}] items.')
+
+    def remove(self, item):
+        """ override remove."""
+        super().remove(item)
+        print(f'Removed [{item}] from the list.')
+
+    def pop(self, index=-1):
+        """ override pop."""
+        item = self[index]
+        super().pop(index)
+        print(f'Popped [{item}] from the list.')
+
