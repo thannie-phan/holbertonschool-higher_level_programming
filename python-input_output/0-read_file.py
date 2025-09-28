910a60c786fefafe9481ec253114ff272edd465d
thannie-phan <thannie-phan@gmail.com>
2025-09-28 22:39:31 +0000

    pycode

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This is a module to read file"""
+
+
 def read_file(filename=""):
     """This is a function to read content of file"""
-    with open (filename) as file:
+    with open(filename) as file:
         content = file.read()
         print(content, end='')
