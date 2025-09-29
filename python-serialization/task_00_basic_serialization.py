a0516e894ee83548b5de8da2c3f7b495ea677eac
thannie-phan <thannie-phan@gmail.com>
2025-09-29 07:06:16 +0000

    task0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,1 +1,18 @@
-eck
+#!/usr/bin/python3
+"""This module defines class student."""
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serializes and save data to the specified file"""
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    """Load and Deserializes data to the specified file"""
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        return data
