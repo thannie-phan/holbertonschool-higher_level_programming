5da51daedae18d95056d23583d5b3e73cadbc5d2
thannie-phan <thannie-phan@gmail.com>
2025-09-29 06:06:10 +0000

    task12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,20 +7,21 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
-    for current_row_index in range(n):
-        current_row = [1]
+    pascals_triangle = []
 
-        previous_row_index = current_row_index - 1
-        if previous_row_index >= 0:
-            previous_row = triangle[previous_row_index]
+    for row_number in range(n):
+        row_numbers = [1]
 
-            for position_in_row in range(1, current_row_index):
-                value = previous_row[position_in_row - 1] + previous_row[position_in_row]
-                current_row.append(value)
+        above_row_number = row_number - 1
+        if above_row_number >= 0:
+            row_above = pascals_triangle[above_row_number]
 
-            current_row.append(1)
+            for position_in_row_numbers in range(1, row_number):
+                number_to_add = row_above[position_in_row_numbers - 1] + row_above[position_in_row_numbers]
+                row_numbers.append(number_to_add)
 
-        triangle.append(current_row)
+            row_numbers.append(1)
 
-    return triangle
+        pascals_triangle.append(row_numbers)
+
+    return pascals_triangle
