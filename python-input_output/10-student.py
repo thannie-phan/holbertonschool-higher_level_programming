83f9edbc826640a1efac1a48ec74b2eb1e6bb6b7
thannie-phan <thannie-phan@gmail.com>
2025-09-29 04:10:03 +0000

    task10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines class student."""
+
+
+class Student:
+    """This defines class student. """
+    pass
+
+    def __init__(self, first_name, last_name, age):
+        """This defines attributes of a student obj."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        result = {}
+        if attrs is None:
+            return self.__dict__
+
+        for key in attrs:
+            if key in self.__dict__:
+                result[key] = self.__dict__[key]
+        return result
