28b1e0a2304e03979ababafb3a47d22e2cabf0d7
thannie-phan <thannie-phan@gmail.com>
2025-09-29 03:14:16 +0000

    task7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module creates on obj from JSON"""
+
+
+import json
+import sys
+import os
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+filename = 'add_item.json'
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
