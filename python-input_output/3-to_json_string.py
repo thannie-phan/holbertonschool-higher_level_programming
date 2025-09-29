33508556966432234626124aeff91ff81fac8ff5
thannie-phan <thannie-phan@gmail.com>
2025-09-29 02:28:42 +0000

    task3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module returns JSON rep of an obj"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """This function returns JSON rep of an obj"""
+    return json.dumps(my_obj)
