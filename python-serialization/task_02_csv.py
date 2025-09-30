061bd5eff1f3543600b93c9246da2e284dcaa8f2
thannie-phan <thannie-phan@gmail.com>
2025-09-30 01:19:33 +0000

    task2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module serialise and deserialise csv content"""
+
+
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    
+    data = []
+    try:
+        with open(filename, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(row)
+        
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file)
+
+        return True
+    
+    except FileNotFoundError:
+        return False
