154c85764a61c0f2f80a9513398fb8ffa99d06c9
thannie-phan <thannie-phan@gmail.com>
2025-10-06 23:36:34 +0000

    task2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -12,7 +12,7 @@ response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
 def fetch_and_print_posts():
     """fetch and print post from json placeholder"""
-    print(f'Status code: {response.status_code}')
+    print(f'Status Code: {response.status_code}')
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
