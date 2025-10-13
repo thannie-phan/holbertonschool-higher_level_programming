2851fcbcfb65a14a44ad793d56295d4709d16bb1
thannie-phan <thannie-phan@gmail.com>
2025-10-13 18:27:45 +1100

    task1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create user_0d_1 with all privilleges
+-- its pw should be set to user_0d_1_pwd
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
