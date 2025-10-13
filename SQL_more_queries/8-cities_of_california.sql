6e8b7eeac27a7d1e7db01918effbec9e8b097d67
thannie-phan <thannie-phan@gmail.com>
2025-10-14 08:31:00 +1100

    task8

---
+++ SQL_more_queries/8-cities_of_california.sql
@@ -0,0 +1,10 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+-- states table contains only one record where name = California (but the id can be different
+-- not allowed to use JOIN
+
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id from states WHERE name = 'California'
+)
+ORDER by id;
