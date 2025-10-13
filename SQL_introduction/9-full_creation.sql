d4212d48a89df143a03d1fcc9684a0d1bdcbc068
thannie-phan <thannie-phan@gmail.com>
2025-10-13 16:04:18 +1100

    task9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- creates a table second_table in database hbtn_0c_0
+-- table has id INT name VARCHAR(256) and score INT
+-- add multiple rows
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (name, score)
+VALUES 
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
