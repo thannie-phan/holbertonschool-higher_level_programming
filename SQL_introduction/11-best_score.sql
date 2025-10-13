d7467d97059f4cd5472777eb9b3ae2f0e35cdbfe
thannie-phan <thannie-phan@gmail.com>
2025-10-13 17:27:56 +1100

    task12

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
 -- list records of second_table in database hbtn_0c_0
+-- ordered by score (top first)
 -- only show those with score >= 10
 
 SELECT score, name
---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+--  updates the score of Bob to 10 second_table in database hbtn_0c_0
+
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
