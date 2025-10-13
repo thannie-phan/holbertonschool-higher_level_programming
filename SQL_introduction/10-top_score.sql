d35719325770cb9b5aa4b70bc92a09615c1bb803
thannie-phan <thannie-phan@gmail.com>
2025-10-13 16:14:38 +1100

    task11

--- SQL_introduction/10-top_score.sql
+++
@@ -1,6 +0,0 @@
--- list all records of second_table in database hbtn_0c_0
--- record display score then name, in order of score. 
-
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- list records of second_table in database hbtn_0c_0
+-- only show those with score >= 10
+
+SELECT score, name
+FROM second_table
+WHERE score >= 10;
