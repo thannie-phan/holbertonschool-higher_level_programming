b0d8900708a93934f23255d0c6a5863762239f02
thannie-phan <thannie-phan@gmail.com>
2025-10-13 15:33:37 +1100

    task4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- creates a table called first_table
+-- id INT
+-- name VARCHAR(256)
+-- if table alr exists, script should not fail
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT AUTO_INCREMENT PRIMARY KEY
+);
