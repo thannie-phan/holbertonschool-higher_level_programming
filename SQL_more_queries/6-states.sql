e2b25f3b8ad6dce5d95b3d77f871cfc8d526dbf9
thannie-phan <thannie-phan@gmail.com>
2025-10-13 19:58:00 +1100

    task6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates table database hbtn_0d_usa and table states (in the database hbtn_0d_usa)
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT AUTO INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
