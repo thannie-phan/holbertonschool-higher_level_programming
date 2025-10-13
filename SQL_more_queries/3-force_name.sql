4b08fcf6eb32031d0f6a9970e9850092c0fe41e7
thannie-phan <thannie-phan@gmail.com>
2025-10-13 19:44:39 +1100

    task3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,7 @@
 -- creates table force_name with id INT and name VARCHAR(256) which cannot be NULL
 
 CREATE TABLE IF NOT EXISTS force_name(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
 
