0bbb8f0058b5bec8aeae1ff5d9143a7a677c0429
thannie-phan <thannie-phan@gmail.com>
2025-10-13 15:47:27 +1100

    task5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- returns the exact SQL used to create the table, including column types, keys, engine, charset,
+
+SHOW CREATE TABLE first_table;
