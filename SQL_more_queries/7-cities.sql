30cfa2dd1b00d984fe46227044c0f28a99be532a
thannie-phan <thannie-phan@gmail.com>
2025-10-14 08:23:43 +1100

    task7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+-- creates table database hbtn_0d_usa and table cities (in the database hbtn_0d_usa)
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+    name VARCHAR(256) NOT NULL
+);
