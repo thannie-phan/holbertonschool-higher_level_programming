3001b6f6322c51c4c74428f6ce8375909c773fdf
thannie-phan <thannie-phan@gmail.com>
2025-10-13 16:05:05 +1100

    task9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 
 INSERT INTO second_table (name, score)
 VALUES 
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+    ('John', 10),
+    ('Alex', 3),
+    ('Bob', 14),
+    ('George', 8);
