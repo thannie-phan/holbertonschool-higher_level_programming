e3ae807f79010cd51453a3aab05a88fa93da57f0
thannie-phan <thannie-phan@gmail.com>
2025-10-23 09:22:19 +1100

    task1

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""this is a python command to filter states"""
+"""this is a python command to list all states in asc order by id"""
 
 import MySQLdb
 import sys
 
 
 def list_all_states():
-    """connect to mysql and filter states."""
+    """connect to mysql and lists all states in asc order by id."""
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
