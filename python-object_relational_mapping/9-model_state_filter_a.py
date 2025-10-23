909fec04e9b7bdc506a4aca2345dcca4b74d41ec
thannie-phan <thannie-phan@gmail.com>
2025-10-23 14:56:49 +1100

    task9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines State class and creates states table in MySQL"""
+
+import MySQLdb
+import sys
+from sqlalchemy import Column, Integer, String, create_engine, engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+            ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+
+    if state_with_a:
+        print("{}: {}".format(state_with_a.id, state_with_a.name))
+    else:
+        print('Nothing')
+
+    session.close()
