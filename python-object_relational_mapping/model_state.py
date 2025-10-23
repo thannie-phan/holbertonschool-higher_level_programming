df28e1b4dfc567f5b02eecfc0be5791de6002225
thannie-phan <thannie-phan@gmail.com>
2025-10-23 13:51:27 +1100

    task6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines State class and creates states table in MySQL"""
+
+
+import MySQLdb
+import sys
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True )
+    name =  Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
