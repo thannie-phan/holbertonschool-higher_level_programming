1da978864515d57238d1b1392217dd5933950288
thannie-phan <thannie-phan@gmail.com>
2025-10-24 07:12:50 +1100

    task14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,9 +7,7 @@ import sys
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
-
+from model_state import Base
 
 class City(Base):
     """This is the class city"""
