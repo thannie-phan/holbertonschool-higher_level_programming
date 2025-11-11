81f4a5a477dabe3807d7bbeb3620031d522638c0
thannie-phan <thannie-phan@gmail.com>
2025-11-11 15:50:53 +1100

    task0 update import

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,6 +1,7 @@
 import os
+from typing import List, Dict
 
-def generate_invitations(template: str, attendees: list[dict]):
+def generate_invitations(template: str, attendees: List[Dict]):
     if not isinstance(template, str):
         print("template must be a string")
         return
