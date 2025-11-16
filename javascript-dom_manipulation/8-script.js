e72c62f0ae053141e6491e1fb97ef2ac929ef356
thannie-phan <thannie-phan@gmail.com>
2025-11-17 08:39:27 +1100

    task8 comment

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -6,3 +6,5 @@ document.addEventListener('DOMContentLoaded', () => {
             sayHi.textContent = data.hello;
         });
 });
+
+//DOMContentLoaded waits until all HTML is parsed then code is run, because the assignment asks for "Your script must work when it is imported from the <head> tag" but at that stage the divid hello has not been loaded yet
